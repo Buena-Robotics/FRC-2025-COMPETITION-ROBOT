3b7e5ed1c4ff8673bc0b614ce04fc721a8bafac2
Illusion137 <raygoza.d@hotmail.com>
2025-01-23 00:56:29 -0700

    slightly better elevator_sim

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC-2025-COMPETITION-ROBOT";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 24;
-    public static final String GIT_SHA = "db6365fa1a2d36530e8af86dae633c83003652c2";
-    public static final String GIT_DATE = "2025-01-22 13:53:50 EST";
+    public static final int GIT_REVISION = 25;
+    public static final String GIT_SHA = "d402cecb388ab6efe0bfa510d85efdeaf50e35fe";
+    public static final String GIT_DATE = "2025-01-22 22:08:11 EST";
     public static final String GIT_BRANCH = "dev_0";
-    public static final String BUILD_DATE = "2025-01-22 22:06:14 EST";
-    public static final long BUILD_UNIX_TIME = 1737601574548L;
+    public static final String BUILD_DATE = "2025-01-23 02:54:18 EST";
+    public static final long BUILD_UNIX_TIME = 1737618858900L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
@@ -5,39 +5,37 @@ import org.littletonrobotics.junction.Logger;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 
 public class ElevatorIOSim implements ElevatorIO {
-    private final double LIFT_SIM_P = 5;
-    private final double LIFT_SIM_D = 0.1;
+    private final double LIFT_SIM_P = 5.0;
+    private final double LIFT_SIM_D = 0.2;
+    private final double LIFT_CARRIAGE_MASS_KG = 2.0;
+    private final double LIFT_DRUM_RADIUS_METERS = Units.inchesToMeters(8);
 
     private final DCMotor lift_gearbox = DCMotor.getNeo550(1);
-    private final DCMotorSim lift_sim = new DCMotorSim(LinearSystemId.createDCMotorSystem(lift_gearbox, 0.004, Elevator.LIFT_MOTOR_REDUCTION), lift_gearbox);
     private final ProfiledPIDController lift_controller = new ProfiledPIDController(LIFT_SIM_P, 0, LIFT_SIM_D, new TrapezoidProfile.Constraints(60, 60));
+
+    private final ElevatorSim lift_sim = new ElevatorSim(lift_gearbox, Elevator.LIFT_MOTOR_REDUCTION, LIFT_CARRIAGE_MASS_KG, LIFT_DRUM_RADIUS_METERS, 0, Units.inchesToMeters(Elevator.ELEVATOR_MAX_HEIGHT_INCHES), false, 0, 0.0, 0.01);
 
     private double lift_applied_volts = 0.0;
     private boolean lift_brake_mode = true;
 
     public ElevatorIOSim() {
-        lift_controller.setTolerance(0.01);
+        lift_controller.setTolerance(0.1);
     }
 
     @Override public void updateInputs(final ElevatorIOInputs inputs) {
-        lift_applied_volts = lift_controller.calculate(lift_sim.getAngularPositionRad());
-        lift_sim.setInputVoltage(MathUtil.clamp(lift_applied_volts, -12.0, 12.0));
+        lift_applied_volts = MathUtil.clamp(lift_controller.calculate(Units.metersToInches(lift_sim.getPositionMeters())), -12.0, 12.0);
+        lift_sim.setInputVoltage(lift_applied_volts);
         lift_sim.update(0.02);
 
-        if (lift_sim.getAngularPositionRad() >= Elevator.ELEVATOR_MAX_HEIGHT_INCHES)
-            lift_sim.setState(Elevator.ELEVATOR_MAX_HEIGHT_INCHES, 0.0);
-        if (lift_sim.getAngularPositionRad() < 0)
-            lift_sim.setState(0, 0.0);
-
         inputs.lift_setpoint_position_inches = lift_controller.getSetpoint().position;
-        inputs.lift_position_inches = lift_sim.getAngularPositionRad();
-        inputs.lift_velocity_inches_per_second = lift_sim.getAngularVelocityRadPerSec();
-        inputs.lift_applied_volts = lift_sim.getInputVoltage();
+        inputs.lift_position_inches = Units.metersToInches(lift_sim.getPositionMeters());
+        inputs.lift_velocity_inches_per_second = Units.metersToInches(lift_sim.getVelocityMetersPerSecond());
+        inputs.lift_applied_volts = lift_applied_volts;
         inputs.lift_current_amps = lift_sim.getCurrentDrawAmps();
         inputs.lift_connected = true;
 
