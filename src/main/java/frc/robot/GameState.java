9b70312db3b8ec24d31abe375da49d72e8f199a0
Illusion137 <raygoza.d@hotmail.com>
2025-02-04 14:01:05 -0700

    trueMaxSpeedCharacterization + robot_state + game_state

---
+++ src/main/java/frc/robot/GameState.java
@@ -0,0 +1,29 @@
+package frc.robot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.util.Reef;
+
+public class GameState {
+    public static Reef reef = new Reef(Config.getRobotAlliance());
+    public static List<Pose2d> corals;
+    public static List<Pose2d> algaes;
+    public static List<Pose2d> robots; // [Assume]: Robots are 30x30 inches perimeter (not including bumpers)
+
+    private GameState(){}
+    
+    public static List<Pair<Translation2d, Translation2d>> getDynamicObstacles(){
+        List<Pair<Translation2d, Translation2d>> obstacles = new ArrayList<>();
+        return obstacles;
+    }
+    public static int bestReefTarget(){
+        return -1;
+    }
+    public static int bestCoralStation(){
+        return -1;
+    }
+}
--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.drive.Drive;
+import frc.robot.util.Printf;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -268,6 +269,17 @@ public class DriveCommands {
         double[] positions = new double[4];
         Rotation2d last_angle = new Rotation2d();
         double gyro_delta = 0.0;
+    }
+
+    public static Command trueMaxDriveSpeedCharacterization(final Drive drive) {
+        return Commands.parallel(
+            Commands.run(() -> {
+                Printf.info("True Max Drive Speed(m/s): %f");
+            }, drive),
+            Commands.sequence(Commands.waitSeconds(3.0), Commands.runOnce(() -> {
+                
+            }))
+        );
     }
 
     private static final PathConstraints pathfinding_constraints = new PathConstraints(5, 3, Units.degreesToRadians(540), Units.degreesToRadians(720));
