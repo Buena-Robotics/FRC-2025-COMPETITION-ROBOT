91b7bee3001a5269f3cf5a8ad0d1127c5e0c8c75
Illusion137 <raygoza.d@hotmail.com>
2025-03-16 14:32:05 -0700

    intake sim

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC-2025-COMPETITION-ROBOT";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 63;
-    public static final String GIT_SHA = "4e6bf844f36f4ca893ce84b75aa9571982792af1";
-    public static final String GIT_DATE = "2025-03-05 02:23:25 EST";
+    public static final int GIT_REVISION = 64;
+    public static final String GIT_SHA = "87ff10052a5f93d47fde33d88d97bb0e62391555";
+    public static final String GIT_DATE = "2025-03-06 02:03:07 EST";
     public static final String GIT_BRANCH = "dev_0";
-    public static final String BUILD_DATE = "2025-03-06 01:59:28 EST";
-    public static final long BUILD_UNIX_TIME = 1741244368461L;
+    public static final String BUILD_DATE = "2025-03-07 11:30:39 EST";
+    public static final long BUILD_UNIX_TIME = 1741365039155L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/mailbox/MailboxIOSim.java
+++ src/main/java/frc/robot/subsystems/mailbox/MailboxIOSim.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems.mailbox;
 
 import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 
+import org.ironmaple.simulation.IntakeSimulation;
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
 import org.ironmaple.simulation.seasonspecific.reefscape2025.ReefscapeCoralOnFly;
@@ -27,10 +29,22 @@ public class MailboxIOSim implements MailboxIO {
 
     private final SwerveDriveSimulation drive_simulation;
     private final Elevator elevator;
+    private final IntakeSimulation intake_simulation;
 
     public MailboxIOSim(final SwerveDriveSimulation drive_simulation, final Elevator elevator) {
         this.drive_simulation = drive_simulation;
         this.elevator = elevator;
+        this.intake_simulation = IntakeSimulation.InTheFrameIntake(
+            // Specify the type of game pieces that the intake can collect
+            "Coral",
+            // Specify the drivetrain to which this intake is attached
+            drive_simulation,
+            // Width of the intake
+            Meters.of(0.4),
+            // The intake is mounted on the back side of the chassis
+            IntakeSimulation.IntakeSide.BACK,
+            // The intake can hold up to 1 coral
+            1);
     }
 
     @Override public void updateInputs(final MailboxIOInputs inputs) {
@@ -55,10 +69,11 @@ public class MailboxIOSim implements MailboxIO {
         inputs.shooter_connected = true;
 
         inputs.coral_beambreak_connected = true;
-        inputs.coral_beam_broken = false;
+        inputs.coral_beam_broken = intake_simulation.getGamePiecesAmount() == 1 && shooter_sim.getAngularPositionRad() >= -10;
 
         if (inputs.shooter_position_radians <= Mailbox.CORAL_END_POSITION) {
             resetPosition();
+            intake_simulation.removeObtainedGamePieces(SimulatedArena.getInstance());
             SimulatedArena.getInstance()
                 .addGamePieceProjectile(new ReefscapeCoralOnFly(
                     // Obtain robot position from drive simulation
